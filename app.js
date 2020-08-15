5ae946c8611b7bc1943014bc9718f9de739ee951
ridhoajibx <ridhoajibx@gmail.com>
2020-08-16 04:21:32 +0700

    Collection element for UI todos

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ const headingTitle = (title) => {
     }
 }
 headingTitle ("Create New Title here!");
+
+// Collect UI element for todos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const filterInput = document.querySelector("#filter-input");
+const todoList = document.querySelector("#todo-list");
+const clearBtn = document.querySelector("#clear-todos");
+console.log(todoForm);
