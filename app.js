a3debd9a2d62e3caa63b2cb425e3877a96b9365b
ridhoajibx <ridhoajibx@gmail.com>
2020-08-16 04:59:37 +0700

    addTodo and DOM create element

--- app.js
+++ app.js
@@ -15,4 +15,25 @@ const todoInput = document.querySelector("#todo-input");
 const filterInput = document.querySelector("#filter-input");
 const todoList = document.querySelector("#todo-list");
 const clearBtn = document.querySelector("#clear-todos");
-console.log(todoForm);
+
+todoForm.addEventListener("submit", addTodo);
+
+function addTodo(e) {
+    e.preventDefault();
+    // Create element li with DOM
+    const li = document.createElement("li");
+    // Add class to element li
+    li.className = "list-group-item d-flex justify-content-between align-items-center mb-1";
+    // Add child (value) to element li
+    li.appendChild(document.createTextNode("Value yang di input"));
+
+    // Create element a with DOM
+    const a = document.createElement("a");
+    a.href = "#";
+    a.className = "badge badge-danger";
+    a.innerHTML = "Delete";
+
+    // input element a to li children
+    li.appendChild(a); 
+    console.log(li);
+}
